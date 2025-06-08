5ea3f52dfeebb92047fb3c9e232d2312719c8fa4
ES-SAM <essamgamesemg@gmail.com>
2025-06-08 04:57:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+// commands usage:
+/*
+1- code reminder when leaving the project for a while without committing:
+2- a refreance for the other developers to understand the project structure and flow.
+3- a way to document the project for future reference.
+*/ 
+####################################################
+
+// mathmatical operations
+/*
++ addition                                                          |                      or += add and assign
+- subtraction                                                    |                      or -= subtract and assign
+* multiplication                                                |                      or *= multiply and assign
+/ division                                                            |                      or /= divide and assign
+% modulus (remainder of division)             |                      or %= modulus and assign
+++ increment (increase by 1)                 
+-- decrement (decrease by 1)
+*/
+#####################################################
+
+// variables
+/*
+Variables are used to store data that can be used later in the code. They can hold different types of data, such as numbers, strings, arrays, and objects.
+
+syntax: $variable_name = value;            (declare a variable with a name and assign it a value.)
+*/
+#####################################################
+
+// data types
+/*
+integer: 42 / 7 / 3 /100
+float: 3.14 / 2.718 / 1.618
+string: "Hello, World!" / 'PHP is fun!'
+boolean: true / false
+array: [1, 2, 3] / ["apple", "banana", "cherry"]
+object: new ClassName() / $object = new stdClass();
+null: null (represents a variable with no value)
+*/
+#####################################################
+
+//printing and output
+/* 
+The `echo` and `print` statements are used to output data to the screen.
+echo "Hello, World!";
+print ("Hello, World!");
+*/
+#####################################################
+
+
+?>
